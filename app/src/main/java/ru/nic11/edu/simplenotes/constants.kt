9b2bed030f62646b2be39d8c5ae2088cb23ea1f8
Roman Nikonov <niconneo@yandex.ru>
2020-05-20 08:41:30 +0300

    public api

--- app/src/main/java/ru/nic11/edu/simplenotes/constants.kt
+++ app/src/main/java/ru/nic11/edu/simplenotes/constants.kt
@@ -1,4 +1,6 @@
 package ru.nic11.edu.simplenotes
 
 const val LOG_TAG = "kis-simplenotes"
-const val API_BASE = "http://10.0.2.2:3000/api"
+// const val API_BASE = "http://10.0.2.2:3000/api"
+const val API_BASE = "http://prometheus.nic11.ru:3000/api"
+
